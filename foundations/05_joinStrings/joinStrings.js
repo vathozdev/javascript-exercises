a69c9ed8f63fa51f24e6b5751867e45991292e62
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-05-12 18:34:38 +0300

    fix: correct greeting construction and separate fullName and age calculations

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -13,8 +13,10 @@ const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
 
-const greeting = "Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.";
-console.log(greeting);
+const fullName = (firstName + lastName);
+const age = (thisYear - birthYear);
+
+const greeting = "Hello! My name is 
 
 
 
