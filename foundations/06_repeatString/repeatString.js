f70a71e43f29fa328f6c26abaa4c08e046b673fd
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-06 16:36:19 +0300

    fix: correct string repetition logic in repeatString function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,7 +1,13 @@
 const repeatString = function(str, num) {
-for (let i=1; i <= num; i++){
+    let result = ""
+    if (num < 0) {
+        return "ERROR";
+    } else {
+    for (let i=1; i <= num; i++){
     repeatedString += str ;
 }
+    }
+
 return repeatString;
 };
 
