a1439fd5e05080b0a4872ad5ff9e49598e11ad66
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-07 14:59:07 +0300

    feat: implement start and end logic in sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,6 +2,16 @@ const sumAll = function(a, b) {
     let result = 0;
     let start = "";
     let end = "";
+    if( a < b ) {
+        start = a;
+        end = b;
+    }   else {
+        start = b;
+        end = a;
+    }
+    for( let i = start; i <= end; i++) {
+        result += i;
+    }
 
 };
 
