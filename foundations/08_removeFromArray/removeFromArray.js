4020312e7eaaf1f38c1e6b29ae22e3ca7d45d3ab
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-07 14:06:21 +0300

    fix: add missing semicolon in removeFromArray function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 const removeFromArray = function(arr, ...thingsToRemove) {
-    let result = arr.filter(item => !thingsToRemove.includes(item))
+    let result = arr.filter(item => !thingsToRemove.includes(item));
+    return result; 
 };
 
 // Do not edit below this line
