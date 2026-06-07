85dc8bf82db04c1fdf58f6948c14d59a837ac814
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-07 15:17:06 +0300

    Add error handling and enable previously skipped tests in sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function(a, b) {
     let result = 0;
     let start = "";
     let end = "";
+    if( typeof a !== "number" || typeof b !== "number" || !Number.isInteger(a) || !Number.isInteger(b) || (a) < 0 || (b) < 0 )  {
+        return "ERROR"
+    }
     if( a < b ) {
         start = a;
         end = b;
@@ -12,7 +15,7 @@ const sumAll = function(a, b) {
     for( let i = start; i <= end; i++) {
         result += i;
     }
-
+    return result;
 };
 
 // Do not edit below this line
