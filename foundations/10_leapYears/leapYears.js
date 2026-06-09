1c726afbf04fc408e18703a9225c25849d4a07e0
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-09 17:10:53 +0300

    Add type check for year input in leapYears function

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,5 @@
 const leapYears = function(year) {
+ if ( typeof year !== "number" ) { return false; }
  if( year % 4 === 0 && year % 100 !== 0) {
     return true;
  }  else if( year % 400 === 0) {
