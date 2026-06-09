bbc88552b14499a2e622806a8525dc0f3dca9643
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-09 17:53:18 +0300

    Add return statement in convertToFahrenheit function to ensure proper output

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -8,6 +8,7 @@ const convertToCelsius = function(fahreinheit) {
 const convertToFahrenheit = function(celcius) {
   let fahreinheit = ( celcius * 1.8 ) + 32;
   let roundedFahrenheit = Math.round( fahreinheit * 10 ) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
