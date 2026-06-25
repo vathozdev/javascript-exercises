ea6d07433a425b59f0bf8fa4cfcd30827e2a8283
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-25 14:06:00 +0300

    Implement palindromes function to accept a string parameter and convert it to lowercase

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let loweredCase = str.toLowerCase();
+    let cleanedStr = loweredCase.replace()
 };
 
 // Do not edit below this line
