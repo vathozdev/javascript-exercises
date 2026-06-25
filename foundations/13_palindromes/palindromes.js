278ba3bb4e0d2d54a86f37b9d8a92e7afec043ec
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-25 14:13:23 +0300

    Fix palindromes function to clean input string by removing spaces

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (str) {
     let loweredCase = str.toLowerCase();
-    let cleanedStr = loweredCase.replace(/[^a-zA-Z0-9\s]/g, "");
+    let cleanedStr = loweredCase.replace(/[^a-zA-Z0-9]/g, "");
     if( cleanedStr === cleanedStr.split("").reverse().join("")) {
         return true;
     } else {    
