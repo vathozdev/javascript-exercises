7d2bd8e7b0ce06d4921920acfbc9b00a92dcf84b
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-25 14:11:00 +0300

    Fix palindromes function test cases to enable all tests and ensure proper functionality

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (str) {
     let loweredCase = str.toLowerCase();
     let cleanedStr = loweredCase.replace(/[^a-zA-Z0-9\s]/g, "");
-    if( cleanedStr === cleanedString.split("").reverse().join("")) {
+    if( cleanedStr === cleanedStr.split("").reverse().join("")) {
         return true;
     } else {    
         return false; 
