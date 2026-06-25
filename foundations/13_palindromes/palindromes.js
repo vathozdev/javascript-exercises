e4701b4241041a41902f11536cb0ad6b7efb32de
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-25 14:07:13 +0300

    Fix palindromes function to compare cleaned string with its reverse

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
 const palindromes = function (str) {
     let loweredCase = str.toLowerCase();
     let cleanedStr = loweredCase.replace(/[^a-zA-Z0-9\s]/g, "");
+    if( cleanedStr === cleanedString.split("").reverse().join("")) {
+        
+    }
 };
 
 // Do not edit below this line
